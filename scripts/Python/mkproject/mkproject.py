244310d452b6d7fae311db6adcd109e859d3b739
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-09-04 19:27:41 -0300

    Add helper functions: fruit_me, log_me

--- scripts/Python/mkproject/mkproject.py
+++ scripts/Python/mkproject/mkproject.py
@@ -8,6 +8,9 @@ Port of 'mkproject' (Bash).
 
 """
 
+import random
+import sys
+
 
 NORMAL = 0
 BOLD = 1
@@ -20,17 +23,16 @@ REVERSE = 7
 HIDDEN = 8
 STRIKETHROUGH = 9
 
-
 COLOUR_0 = BLACK = 30
 COLOUR_1 = RED = 31
-COLOUR_2 = green =  32
-COLOUR_3 = yellow =  33
-COLOUR_4 = blue = 34
-COLOUR_5 = pink =  35
-COLOUR_6 = cyan =  36
-COLOUR_7 = grey = 37
-COLOUR_8 = white =  38
-COLOUR_DEFAULT = default_fg =39
+COLOUR_2 = GREEN = 32
+COLOUR_3 = YELLOW = 33
+COLOUR_4 = BLUE = 34
+COLOUR_5 = PINK = 35
+COLOUR_6 = CYAN = 36
+COLOUR_7 = GREY = 37
+COLOUR_8 = WHITE = 38
+COLOUR_DEFAULT = DEFAULT_FG = 39
 
 
 def ansify(codes: list[int]) -> str:
@@ -42,3 +44,22 @@ def ansify(codes: list[int]) -> str:
     elems.append("m")
 
     return "".join(elems)
+
+
+def fruit_me() -> str:
+    fruits = ["🍒" "🍑" "🍌" "🍎" "🍏" "🍇" "🍊" "🍍" "🥝" "🍉" "🍈" "🍋" "🍐" "🍓"
+              ]
+    return random.choice(fruits)
+
+
+def log_me(message: str, level: str) -> None:
+    match level:
+        case "info":
+            sys.stdout.write(
+                f"{ansify([BLUE])} {ansify([ITALIC, GREEN])}{message}"
+            )
+        case "warning":
+            sys.stdout.write(
+                f"{ansify([YELLOW])} {ansify([ITALIC, RED])}{message}"
+            )
+    sys.stdout.write(f"{ansify([NORMAL])}\n")
